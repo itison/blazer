833a78d7a6dfcb5645dde4901f14b46ded78505f
Colin Frame <colin.frame@itison.com>
2025-09-09 13:30:49 +0100

    Fixing bugs

--- app/helpers/blazer/completion_helper.rb
+++ app/helpers/blazer/completion_helper.rb
@@ -12,7 +12,7 @@ module Blazer
     end
 
     def blazer_table_name_completion_source
-      extract_tables_and_columns(Blazer.data_sources['main'].schema).collect do |entry|
+      extract_tables_and_columns(Blazer.data_sources.first.last.schema).collect do |entry|
         {
           value: entry.first,
           columns: entry.last.map do |column|
